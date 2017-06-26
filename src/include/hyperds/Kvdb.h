6a94c9fba2742c04c531dbb92b0bec63530b82e3
yangwanyuan <ywy8861@126.com>
2017-06-26 09:08:53 +0800

    change license

--- src/include/hyperds/Kvdb.h
+++ src/include/hyperds/Kvdb.h
@@ -1,8 +1,3 @@
-//  Copyright (c) 2017-present, Intel Corporation.  All rights reserved.
-//  This source code is licensed under the BSD-style license found in the
-//  LICENSE file in the root directory of this source tree. An additional grant
-//  of patent rights can be found in the PATENTS file in the same directory.
-
 #ifndef KV_DB_KVDB_H_
 #define KV_DB_KVDB_H_
 
--- test/test_db.cc
+++ test/test_db.cc
@@ -1,8 +1,3 @@
-//  Copyright (c) 2017-present, Intel Corporation.  All rights reserved.
-//  This source code is licensed under the BSD-style license found in the
-//  LICENSE file in the root directory of this source tree. An additional grant
-//  of patent rights can be found in the PATENTS file in the same directory.
-
 #include <string>
 #include <iostream>
 #include "test_base.h"
