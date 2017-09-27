5ea4f15ca5f695fc159d1a310246c28833056cb2
yangwanyuan <ywy8861@126.com>
2017-09-27 13:28:15 +0800

    WorkQueue code clean

--- src/include/WorkQueue.h
+++ src/include/WorkQueue.h
@@ -37,33 +37,31 @@ public:
 
     QueueType Wait_Dequeue(int msec = 1000) {
         std::unique_lock < std::mutex > lck(mtx_);
-if    (cv_.wait_for(lck, std::chrono::milliseconds(msec), [this] {return !dataQueue_.empty();}))
-                    {
-                        QueueType data = dataQueue_.front();
-                        dataQueue_.pop();
-                        return data;
-                    }
-                    else
-                    {
-                        return NULL;
-                    }
-                }
+        if (cv_.wait_for(lck, std::chrono::milliseconds(msec), [this] {return !dataQueue_.empty();})) {
+            QueueType data = dataQueue_.front();
+            dataQueue_.pop();
+            return data;
+        }
+        else {
+            return NULL;
+        }
+    }
 
-                bool empty() {
-                    std::lock_guard<std::mutex> lck(mtx_);
-                    return dataQueue_.empty();
-                }
+    bool empty() {
+        std::lock_guard<std::mutex> lck(mtx_);
+        return dataQueue_.empty();
+    }
 
-                uint32_t length()
-                {
-                    return dataQueue_.size();
-                }
+    uint32_t length()
+    {
+        return dataQueue_.size();
+    }
 
-            private:
-                std::queue<QueueType> dataQueue_;
-                mutable std::mutex mtx_;
-                std::condition_variable cv_;
+private:
+    std::queue<QueueType> dataQueue_;
+    mutable std::mutex mtx_;
+    std::condition_variable cv_;
 
-            };
+};
 }
 #endif //#ifndef _HLKVDS_WORKQUEUE_H_
