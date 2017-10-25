300bfc3e4e5ec90a29536b40cf671eed14e1d8d3
yangwanyuan <wanyuan.yang@intel.com>
2017-10-25 11:22:27 +0800

    modify SuperBlock in SuperBlockManager

--- src/SuperBlockManager.cc
+++ src/SuperBlockManager.cc
@@ -13,7 +13,7 @@ bool SuperBlockManager::Get(char* buff, uint64_t length) {
     if (length != SuperBlockManager::GetSuperBlockSizeOnDevice()) {
         return false;
     }
-    memcpy((void *)buff, (const void*)sb_, SuperBlockManager::SizeOfDBSuperBlock());
+    memcpy((void *)buff, (const void*)&sb_, SuperBlockManager::SizeOfDBSuperBlock());
     return true;
 }
 
@@ -21,24 +21,23 @@ bool SuperBlockManager::Set(char* buff, uint64_t length) {
     if (length != SuperBlockManager::GetSuperBlockSizeOnDevice()) {
         return false;
     }
-    sb_ = new DBSuperBlock;
-    memcpy((void*)sb_, (const void*)buff, SuperBlockManager::SizeOfDBSuperBlock());
+    memcpy((void*)&sb_, (const void*)buff, SuperBlockManager::SizeOfDBSuperBlock());
     return true;
 }
 
 void SuperBlockManager::SetSuperBlock(DBSuperBlock& sb) {
     std::lock_guard < std::mutex > l(mtx_);
-    sb_->hashtable_size = sb.hashtable_size;
-    sb_->number_elements = sb.number_elements;
-    sb_->segment_size = sb.segment_size;
-    sb_->number_segments = sb.number_segments;
-    sb_->current_segment = sb.current_segment;
-    sb_->db_sb_size = sb.db_sb_size;
-    sb_->db_index_size = sb.db_index_size;
-    sb_->db_seg_table_size = sb.db_seg_table_size;
-    sb_->db_data_region_size = sb.db_data_region_size;
-    sb_->device_capacity = sb.device_capacity;
-    sb_->data_theory_size = sb.data_theory_size;
+    sb_.hashtable_size = sb.hashtable_size;
+    sb_.number_elements = sb.number_elements;
+    sb_.segment_size = sb.segment_size;
+    sb_.number_segments = sb.number_segments;
+    sb_.current_segment = sb.current_segment;
+    sb_.db_sb_size = sb.db_sb_size;
+    sb_.db_index_size = sb.db_index_size;
+    sb_.db_seg_table_size = sb.db_seg_table_size;
+    sb_.db_data_region_size = sb.db_data_region_size;
+    sb_.device_capacity = sb.device_capacity;
+    sb_.data_theory_size = sb.data_theory_size;
 }
 
 uint64_t SuperBlockManager::GetSuperBlockSizeOnDevice() {
@@ -48,26 +47,25 @@ uint64_t SuperBlockManager::GetSuperBlockSizeOnDevice() {
 }
 
 SuperBlockManager::SuperBlockManager(Options &opt) :
-    sb_(NULL), options_(opt) {
+    options_(opt) {
 }
 
 SuperBlockManager::~SuperBlockManager() {
-    delete sb_;
 }
 
 void SuperBlockManager::SetElementNum(uint32_t num) {
     std::lock_guard < std::mutex > l(mtx_);
-    sb_->number_elements = num;
+    sb_.number_elements = num;
 }
 
 void SuperBlockManager::SetCurSegId(uint32_t id) {
     std::lock_guard < std::mutex > l(mtx_);
-    sb_->current_segment = id;
+    sb_.current_segment = id;
 }
 
 void SuperBlockManager::SetDataTheorySize(uint64_t size) {
     std::lock_guard < std::mutex > l(mtx_);
-    sb_->data_theory_size = size;
+    sb_.data_theory_size = size;
 }
 
 } // namespace hlkvds
--- src/include/SuperBlockManager.h
+++ src/include/SuperBlockManager.h
@@ -104,47 +104,47 @@ public:
 
     void SetSuperBlock(DBSuperBlock& sb);
     const DBSuperBlock& GetSuperBlock() const {
-        return *sb_;
+        return sb_;
     }
 
     bool IsElementFull() const {
-        return sb_->number_elements == sb_->hashtable_size;
+        return sb_.number_elements == sb_.hashtable_size;
     }
     uint32_t GetMagic() const {
-        return sb_->magic_number;
+        return sb_.magic_number;
     }
     uint32_t GetHTSize() const {
-        return sb_->hashtable_size;
+        return sb_.hashtable_size;
     }
     uint32_t GetElementNum() const {
-        return sb_->number_elements;
+        return sb_.number_elements;
     }
     uint32_t GetSegmentSize() const {
-        return sb_->segment_size;
+        return sb_.segment_size;
     }
     uint32_t GetSegmentNum() const {
-        return sb_->number_segments;
+        return sb_.number_segments;
     }
     uint32_t GetCurSegmentId() const {
-        return sb_->current_segment;
+        return sb_.current_segment;
     }
     uint64_t GetSbSize() const {
-        return sb_->db_sb_size;
+        return sb_.db_sb_size;
     }
     uint64_t GetIndexSize() const {
-        return sb_->db_index_size;
+        return sb_.db_index_size;
     }
     uint64_t GetSegTableSize() const {
-        return sb_->db_seg_table_size;
+        return sb_.db_seg_table_size;
     }
     uint64_t GetDataRegionSize() const {
-        return sb_->db_data_region_size;
+        return sb_.db_data_region_size;
     }
     uint64_t GetDeviceCapacity() const {
-        return sb_->device_capacity;
+        return sb_.device_capacity;
     }
     uint64_t GetDataTheorySize() const {
-        return sb_->data_theory_size;
+        return sb_.data_theory_size;
     }
 
     void SetElementNum(uint32_t num);
@@ -155,11 +155,9 @@ public:
     ~SuperBlockManager();
 
 private:
-    DBSuperBlock* sb_;
+    DBSuperBlock sb_;
 
     Options &options_;
-
-    uint64_t startOff_;
 
     std::mutex mtx_;
 
