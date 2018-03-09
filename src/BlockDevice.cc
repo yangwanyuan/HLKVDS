7ab31802201280eb424794c90d1a34b86df8a8eb
yangwanyuan <wanyuan.yang@intel.com>
2018-03-09 09:39:51 +0800

    make pmemdevice work based on lastest pmdk

--- src/BlockDevice.cc
+++ src/BlockDevice.cc
@@ -9,12 +9,13 @@ BlockDevice* BlockDevice::CreateDevice(std::string path) {
 
     if (type == "kernel") {
         int is_pmem = 0;
-        void *addr = pmem_map_file(path.c_str(), 1024*1024, PMEM_FILE_EXCL, O_RDONLY, NULL, &is_pmem);
+        size_t map_len = 0;
+        void *addr = pmem_map_file(path.c_str(), 0, PMEM_FILE_EXCL, O_RDONLY, &map_len, &is_pmem);
         if (addr != NULL) {
             if (is_pmem) {
                 type = "pmem";
             }
-            pmem_unmap(addr, 1024*1024);
+            pmem_unmap(addr, map_len);
         }
     }
 
